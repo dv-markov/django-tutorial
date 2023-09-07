fdc133f1913b760a1556436b9eadd411e5f8906e
dv-markov <dmitry.v.markov@gmail.com>
2023-09-07 23:58:43 +0300

    play with HttpRequest

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseForbidden, JsonResponse, StreamingHttpResponse
 
 
+def index(request):
+    return HttpResponse("Hello, world. You are in the polls index.")
+
+
+def index2(request):
+    print_http_request(request)
+    response = generate_http_response(MyNumbers())
+    return response
+
+
 def print_http_request(request):
     print("HttpRequest headers:\n", request.headers)
     print("HttpRequest scheme:\n", request.scheme)
@@ -23,6 +33,34 @@ def print_http_request(request):
     # print("HttpRequest get_signed_cookie():\n", request.get_signed_cookie("name"))  # need security - HTTPS, etc
 
 
+def generate_http_response(content=None):
+    # response = HttpResponse("Hello, world! You are in the polls index.")  # минимальный ответ по умолчанию
+    # response = HttpResponse("Text only, please.", content_type="text/plain")  # возвращает текст в теге <pre>
+    # response = HttpResponse(b"Bytestrings are also accepted.")  # отображается как обычный текст
+    response = HttpResponse(memoryview(b"Memoryview as well."))  # отображается как обычный текст
+
+    # works with iterators, string or number
+    # response = HttpResponse(content) if content else HttpResponse()
+
+    # response = StreamingHttpResponse(content)  # Streaming type of content
+
+    response.write("<p>Hello World!<p>")  # в response можно писать, как в файл
+    response.write("<p>How are you doing?<p>")
+
+    response.headers["Age"] = 120  # работа с headers HttpResponse
+    del response.headers["Age"]
+    response["Age"] = 120  # equal, proxies to HttpResponse.headers
+    # del response["Age"]
+    response["Worker"] = "Petrov"
+
+    # returns JSON
+    response2 = JsonResponse({"foo": "bar"})
+    response2.write({"привет": "Петя"})
+    response2.write({"privet": "vasya"})
+
+    return response
+
+
 class MyNumbers:
     def __iter__(self):
         self.a = 1
@@ -35,33 +73,3 @@ class MyNumbers:
             return str(x) + ' '
         else:
             raise StopIteration
-
-
-def generate_http_response(content=None):
-    # response = HttpResponse("Hello, world! You are in the polls index.")  # минимальный ответ по умолчанию
-    # response = HttpResponse("Text only, please.", content_type="text/plain")  # возвращает текст в теге <pre>
-    # response = HttpResponse(b"Bytestrings are also accepted.")  # отображается как обычный текст
-    # response = HttpResponse(memoryview(b"Memoryview as well."))  # отображается как обычный текст
-
-    # works with iterators, string or number
-    response = HttpResponse(content) if content else HttpResponse()
-
-    # response = StreamingHttpResponse(content)  # Streaming type of content
-
-    response.write("<p>Hello World!<p>")  # в response можно писать, как в файл
-    response.write("<p>How are you doing?<p>")
-
-    response.headers["Age"] = 120  # работа с headers HttpResponse
-    del response.headers["Age"]
-    response["Age"] = 120  # equal, proxies to HttpResponse.headers
-    del response["Age"]
-    response["Worker"] = "Petrov"
-
-    # response2 = JsonResponse({"foo": "bar"})  # returns JSON
-    return response
-
-
-def index(request):
-    print_http_request(request)
-    response = generate_http_response(MyNumbers())
-    return response
