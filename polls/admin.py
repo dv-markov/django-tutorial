367779d895506342916f17c3776c2bd971e1d901
dv-markov <dmitry.v.markov@gmail.com>
2023-09-17 18:21:18 +0500

    lesson 4 - add form for detail template, add result view and template

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,20 @@ from django.contrib import admin
 
 from . import models
 
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'question',
+        'choice_text',
+        'votes',
+    )
+    list_display_links = (
+        'id',
+        'question',
+        'choice_text',
+    )
+
+
 admin.site.register(models.Question)
-admin.site.register(models.Choice)
+admin.site.register(models.Choice, ChoiceAdmin)
