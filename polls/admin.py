dd2d6a4304ee35e289240a9998364fe78062b78f
dv-markov <dmitry.v.markov@gmail.com>
2023-09-21 12:33:35 +0500

    lesson 7 - adjust admin panel

--- polls/admin.py
+++ polls/admin.py
@@ -3,6 +3,27 @@ from django.contrib import admin
 from . import models
 
 
+class ChoiceInline(admin.TabularInline):
+    model = models.Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"],
+                              "classes": ["collapse"]}),
+    )
+    inlines = [ChoiceInline]
+    list_display = (
+        "question_text",
+        "pub_date",
+        "was_published_recently",
+    )
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+
 class ChoiceAdmin(admin.ModelAdmin):
     list_display = (
         'id',
@@ -17,5 +38,5 @@ class ChoiceAdmin(admin.ModelAdmin):
     )
 
 
-admin.site.register(models.Question)
-admin.site.register(models.Choice, ChoiceAdmin)
+admin.site.register(models.Question, QuestionAdmin)
+# admin.site.register(models.Choice, ChoiceAdmin)
